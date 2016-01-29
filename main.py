572d1a224416bd5ff6cb202f0d77a1db459af4ad
Your Name <you@example.com>
2016-01-29 08:48:53 -0800

    new file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import subprocess
 import requests
 import sys
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
+BASE_DIR = os.path.dirname(sys.argv[0])
 
 if os.environ.get("DEBUG"):
     BASE_URL ="http://localhost:8001/ctrl/s/play/{}"
@@ -11,13 +11,18 @@ else:
     BASE_URL ="http://nekoanimedd.com/ctrl/s/play/{}"
 
 
-def main(id):
-    url = BASE_URL.format(id) 
+def main(neko_id):
+    _, id = neko_id.split(":")
+    url = BASE_URL.format(id)
     res = requests.get(url)
     url = res.content
-    cmd = ["megadl", "{}".format(url), "--path","-","|", "mpv", "-"]
+    cmd = ['"{}\mega\megadl.exe"'.format(BASE_DIR), '"{}"'.format(url), "--path","-","|", '"{}\mpv\mpv.com"'.format(BASE_DIR), "-"]
+    print " ".join(cmd)
     subprocess.check_output(" ".join(cmd), shell=True)
 
 if __name__ == "__main__":
     main(sys.argv[1])
+
+
+
 
--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
 import py2exe
 
-
-setup(console=["main.py"])
+setup(
+options={'py2exe':{'bundle_files':1, 'compressed':True}},
+console=["main.py"],
+zipfile=None,
+)
