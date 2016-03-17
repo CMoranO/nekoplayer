30d0206a5db793e07dd856d9ef52e4dc98347ff0
Andres Vargas <andres.vargas@interalia.net>
2016-03-17 09:20:26 -0600

    remove timeout

--- small-webbrowser_old.py
+++ small-webbrowser_old.py
@@ -70,7 +70,7 @@ def neko(neko_id='neko:v2Yr'):
     p1.setStandardOutputProcess(p2)
     p1.start('{}'.format(mega_path), ['{}'.format(mega_url),"--path","-"])
     p2.start('{}'.format(mpv_path), ["-" , '--no-terminal'])
-    p2.waitForFinished()
+    p2.waitForFinished(-1)
     #print p2.readAll()
 #           "--path","-",)
     #cmd = ['"{}"'.format(mega_path), '"{}"'.format(mega_url),
